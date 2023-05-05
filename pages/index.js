ffd71b7e5ac07ccbb5798126751a0185048c233a
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-05-05 16:24:50 -0400

    Feat: Responsive behavior of home page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,25 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head'
+import Link from 'next/link'
 export default function Home() {
   return (
     <>
       <Head>
-        <title> Fuel Prices </title>{" "}
+        <title> Fuel Prices </title>{' '}
         <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          name='viewport'
+          content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
         />
-      </Head>{" "}
+      </Head>{' '}
       <main>
-        <div className="welcome">
-          <h1 className="get-started"> GET STARTED </h1>{" "}
-          <button className="open-btn">
-            <Link href="main" className="link">
-              {" "}
-              OPEN MAP{" "}
-            </Link>{" "}
-          </button>{" "}
-        </div>{" "}
-      </main>{" "}
+        <div className='welcome-page'>
+          <h1 className='get-started'> GET STARTED </h1>{' '}
+          <button className='open-btn'>
+            <Link href='main' className='link'>
+              OPEN MAP{' '}
+            </Link>{' '}
+          </button>{' '}
+        </div>{' '}
+      </main>{' '}
     </>
-  );
+  )
 }
